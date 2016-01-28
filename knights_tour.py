a6ac26db0485802c357a8976140bb765a3247f04
lnara002 <lakshman_kumar.narayanan@alcatel-lucent.com>
2016-01-28 09:51:28 -0800

    allow rectangular boards

--- knights_tour.py
+++ knights_tour.py
@@ -4,18 +4,20 @@ from __future__ import print_function
 import sys
 
 tried_so_far = 0
-size = 8
+n_rows = 6
+n_cols = 7
 
 class Board:
-  def __init__(self, size):
-    self.size = size
-    self.board = [-1]*(size*size)
+  def __init__(self, n_rows, n_cols):
+    self.n_rows = n_rows
+    self.n_cols = n_cols
+    self.board = [-1]*(n_rows*n_cols)
 
   def __repr__(self):
     result = ""
     ind = 0
-    for i in range(size):
-      for j in range(size):
+    for i in range(n_rows):
+      for j in range(n_cols):
         if self.board[ind] == -1:
           result += '--'
         else:
@@ -25,33 +27,31 @@ class Board:
       result += '\n'
     return result
 
-def build_next_moves(size):
+def build_next_moves(n_rows,n_cols):
   next_moves = []
-  for i in range(size*size):
-    #number, left, up, down, right
+  for i in range(n_rows*n_cols):
+    #number,                     left, up, down, right
     possible_moves =\
-      [   (-1-(2*size), 1,  2, 0, 0),\
-          ( 1-(2*size), 0,  2, 0, 1),\
-          (-2-size,     2,  1, 0, 0),\
-          ( 2-size,     0,  1, 0, 2),\
-          (-2+size,     2,  0, 1, 0),\
-          ( 2+size,     0,  0, 1, 2),\
-          (-1+(2*size), 1,  0, 2, 0),\
-          ( 1+(2*size), 0,  0, 2, 1) ]
-    next_moves.append([a[0]+i for a in possible_moves if (((i%size)-a[1] >= 0) and \
-                                               ((i%size)+a[4] < size) and \
-                                               ((i/size)-a[2] >= 0) and \
-                                               ((i/size)+a[3] < size)) ])
+      [   (-1-(2*n_cols),        1,  2, 0, 0),\
+          ( 1-(2*n_cols),        0,  2, 0, 1),\
+          (-2-n_cols,            2,  1, 0, 0),\
+          ( 2-n_cols,            0,  1, 0, 2),\
+          (-2+n_cols,            2,  0, 1, 0),\
+          ( 2+n_cols,            0,  0, 1, 2),\
+          (-1+(2*n_cols),        1,  0, 2, 0),\
+          ( 1+(2*n_cols),        0,  0, 2, 1) ]
+    next_moves.append([a[0]+i for a in possible_moves if (((i%n_cols)-a[1] >= 0) and \
+                                               ((i%n_cols)+a[4] < n_cols) and \
+                                               ((i/n_cols)-a[2] >= 0) and \
+                                               ((i/n_cols)+a[3] < n_rows)) ])
   return next_moves
 
 def next_steps(board_right_now,square,next_moves):
-  global size
   return (a for a in next_moves[square] if board_right_now[a] == -1)
 
 def track(board, next_moves, current_square, covered):
   global tried_so_far
-  global size
-  if covered == (size*size):
+  if covered == board.n_rows*board.n_cols:
     return 1
   for i in next_steps(board.board, current_square, next_moves):
     board.board[i] = covered+1
@@ -62,9 +62,9 @@ def track(board, next_moves, current_square, covered):
   return 0
 
 if __name__ == '__main__':
-  b = Board(size)
-  next_moves = build_next_moves(size)
-  for i in range(size*size):
+  b = Board(n_rows,n_cols)
+  next_moves = build_next_moves(n_rows,n_cols)
+  for i in range(n_rows*n_cols):
     print("next moves for %02d is %s"%(i,next_moves[i]))
   b.board[0] = 1
   result = track(b, next_moves, 0, 1)
