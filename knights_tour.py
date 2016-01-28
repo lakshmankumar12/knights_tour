bd057248cfedf525ebeacfa263cdda60e8da841b
lnara002 <lakshman_kumar.narayanan@alcatel-lucent.com>
2016-01-28 09:41:50 -0800

    build next moves just once

--- knights_tour.py
+++ knights_tour.py
@@ -25,45 +25,49 @@ class Board:
       result += '\n'
     return result
 
-def next_moves(i, size):
-   #number, left, up, down, right
-  possible_moves =\
-    [   (-1-(2*size), 1,  2, 0, 0),\
-        ( 1-(2*size), 0,  2, 0, 1),\
-        (-2-size,     2,  1, 0, 0),\
-        ( 2-size,     0,  1, 0, 2),\
-        (-2+size,     2,  0, 1, 0),\
-        ( 2+size,     0,  0, 1, 2),\
-        (-1+(2*size), 1,  0, 2, 0),\
-        ( 1+(2*size), 0,  0, 2, 1) ]
-  return [a[0]+i for a in possible_moves if (((i%size)-a[1] >= 0) and \
-                                             ((i%size)+a[4] < size) and \
-                                             ((i/size)-a[2] >= 0) and \
-                                             ((i/size)+a[3] < size)) ]
+def build_next_moves(size):
+  next_moves = []
+  for i in range(size*size):
+    #number, left, up, down, right
+    possible_moves =\
+      [   (-1-(2*size), 1,  2, 0, 0),\
+          ( 1-(2*size), 0,  2, 0, 1),\
+          (-2-size,     2,  1, 0, 0),\
+          ( 2-size,     0,  1, 0, 2),\
+          (-2+size,     2,  0, 1, 0),\
+          ( 2+size,     0,  0, 1, 2),\
+          (-1+(2*size), 1,  0, 2, 0),\
+          ( 1+(2*size), 0,  0, 2, 1) ]
+    next_moves.append([a[0]+i for a in possible_moves if (((i%size)-a[1] >= 0) and \
+                                               ((i%size)+a[4] < size) and \
+                                               ((i/size)-a[2] >= 0) and \
+                                               ((i/size)+a[3] < size)) ])
+  return next_moves
 
-def next_steps(board_right_now,square):
+def next_steps(board_right_now,square,next_moves):
   global size
-  return (a for a in next_moves(square, size) if board_right_now[a] == -1)
+  return (a for a in next_moves[square] if board_right_now[a] == -1)
 
-def track(board, current_square, covered):
+def track(board, next_moves, current_square, covered):
   global tried_so_far
   global size
   if covered == (size*size):
     return 1
-  for i in next_steps(board.board, current_square):
+  for i in next_steps(board.board, current_square, next_moves):
     board.board[i] = covered+1
     tried_so_far += 1
-    if track(board, i, covered + 1) == 1:
+    if track(board, next_moves, i, covered + 1) == 1:
       return 1
     board.board[i] = -1
   return 0
 
 if __name__ == '__main__':
   b = Board(size)
+  next_moves = build_next_moves(size)
   for i in range(size*size):
-    print("next moves for %02d is %s"%(i,next_moves(i,size)))
+    print("next moves for %02d is %s"%(i,next_moves[i]))
   b.board[0] = 1
-  result = track(b, 0, 1)
+  result = track(b, next_moves, 0, 1)
   if result == 1:
     print(b)
   else:
